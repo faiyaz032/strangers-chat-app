e9969069ce48c49ed2d4715eb424fbb304a50b90
faiyaz032 <faiyazrahman03@gmail.com>
2023-10-04 20:43:38 +0600

    Establish socket connection between client and server
    - Install socket.io-client in the client
    - Install socket.io in the server
    - Configure socket in both client and server

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 import Text from './Text';
 import TextInput from './TextInput';
 
 export default function Chat() {
+  const [socket, setSocket] = useState(null);
+  const [socketId, setSocketId] = useState(null);
+
+  // Establish a socket.io connection to the server
+  useEffect(() => {
+    const connection = io('http://localhost:5000');
+    setSocket(connection);
+    setSocketId(connection.id);
+  }, []);
+
   return (
     <div className="messaging-container">
       {/* Display messages or a message indicating no messages */}
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const httpServer = http.createServer(app);
+
+// Create a Socket.IO server with CORS configuration
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173', // Define the allowed origin for socket connections
+  },
+});
+
+io.on('connection', socket => {
+  console.log(`socket-id:${socket.id}`);
+});
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server is alive on PORT:${PORT}`));
+httpServer.listen(PORT, () => console.log(`Server is alive on PORT:${PORT}`));
